aec7ed4d249480ddc4442a66264cc9e3de981390
chunnnnnnnn <sj985326@gmail.com>
2018-06-20 20:23:36 +0800

    First commit

---
+++ ClubCourse/topic.js
@@ -0,0 +1,23 @@
+var topic = [
+    "高年級實習生",
+    "今夜，在浪漫劇場與你相遇",
+    "樂來樂愛你",
+    "購物狂的異想世界",
+    "今生是第一次",
+    "職場求愛記",
+    "我的初戀情人",
+    "好想告訴你",
+];
+
+var startDate = new Date();
+
+function setMonthAndDay(startMonth, startDay)
+{
+    startDate.setMonth(startMonth-1);
+    startDate.setDate(startDay);
+    startDate.setHours(0);
+    startDate.setMinutes(0);
+    startDate.setSeconds(0);
+}
+
+setMonthAndDay(7,1);
---
+++ YoutubePlayer/playList.js
@@ -0,0 +1,29 @@
+//Youtube 影片 ID
+var playList = [
+    "rRzxEiBLQCA",
+    "LF-tHnkZMnM",
+    "NyXApXudtaU",
+    "UL4RplV2Ugo",
+    "DJnexzGhNAw",
+    "E9maPHhgPjc",
+    "EjWAdKWEVUE",
+    "Cltc9moMfqg",
+    "POqzTqeQ8rM",
+    "i0p1bmr0EmE"
+];
+
+//播放起迄秒數
+var playTime = [
+    [48,75],
+    [130,190],
+    [153,193],
+    [53,90],
+    [12,71],
+    [55,80],
+    [188,265],
+    [166,196],
+    [10,66],
+    [176,217]
+    
+    
+];
